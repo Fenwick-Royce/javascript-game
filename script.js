a7b2ade3dc3cedc33f3336af42a44bc5ec1ecd2c
Fenwick-Royce <Fenwick.Royce@gmail.com>
2022-09-26 10:37:32 -0700

    minor tweak

--- script.js
+++ script.js
@@ -7,3 +7,13 @@ function jump(){
         character.classList.remove("animate");
     },300);
 }
+
+var checkDead = setInterval(function() {
+    let characterTop = parseInt(window.getComputerStyle(character).getPropertyValue("top"));
+    let blockLeft = parseInt(window.getComputerStyle(block).getPropertyValue("left"));
+    if(blockLeft<20 && blockLeft>0 && characterTop>=130){
+        block.style.animation = "none";
+        block.style.display = "none";
+        alert("try again!");
+    }
+}, 10);
